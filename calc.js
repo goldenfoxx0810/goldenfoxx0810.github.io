62a911be5111f169794269690d380aff88fb5ee3
goldenfoxx0810 <56978526+goldenfoxx0810@users.noreply.github.com>
2019-10-28 19:43:16 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -3,11 +3,12 @@ function updatePrice() {
   let select = s[0];
   let price = 0;
   let prices = getPrices();
+  let buf1 = 0;
+  let buf2 = 0;
   let priceIndex = parseInt(select.value) - 1;
   if (priceIndex >= 0) {
     price = prices.prodTypes[priceIndex];
   }
-
   let radioDiv = document.getElementById("rad");
   if (select.value == "4"|| select.value=="2"||select.value == "7")  radioDiv.style.display="block"; 
   else radioDiv.style.display="none";
@@ -17,10 +18,10 @@ function updatePrice() {
       let optionPrice = prices.prodOptions[radio.value];
       if (optionPrice !== undefined) {
         price += optionPrice;
+        buf1 += optionPrice;
       }
     }
   });
-
   let checkDiv = document.getElementById("check");
   if (select.value == "1"||select.value == "3")  checkDiv.style.display="block";
    else checkDiv.style.display ="none";
@@ -30,9 +31,16 @@ function updatePrice() {
       let propPrice = prices.prodProperties[checkbox.name];
       if (propPrice !== undefined) { 
         price += propPrice;
+        buf2 += propPrice;
       }
     }
   });
+  if (select.value == "5"||select.value == "6")
+  price=price-buf1-buf2;
+  if (select.value == "1"||select.value == "3")
+  price=price-buf1;
+  if (select.value == "4"|| select.value=="2"||select.value == "7")
+  price=price-buf2;
 
   let num=document.getElementById("numofclass").value;
     if (typeof num==='undefined') num=0;
@@ -40,8 +48,8 @@ function updatePrice() {
 
   let prodPrice = document.getElementById("prodPrice");
   prodPrice.innerHTML = price + " рублей";
+  
 }
-
 function getPrices() {
   return {
     prodTypes: [800, 600, 1000, 700, 750, 500, 650],
